b7f2694d0d59bbdf9a5e13b8431608c56efca066
youn0592 <youn0592@algonquinlive.com>
2021-09-26 15:38:00 -0400

    Fixed Build Error

--- Source/TempCapstoneProject/TempCapstoneProjectCharacter.cpp
+++ Source/TempCapstoneProject/TempCapstoneProjectCharacter.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "TempCapstoneProjectCharacter.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
 #include "Camera/CameraComponent.h"
 #include "Components/BoxComponent.h"
 #include "Components/CapsuleComponent.h"
